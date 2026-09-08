86e63e4b22d2db99dd30949a1a750f8027033bee
Bolo Michelin <72155+bolom@users.noreply.github.com>
2026-09-08 11:07:08 -0400

    Let administrators manage admin users from the back office

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,54 @@
+module Admin
+  class UsersController < BaseController
+    def index
+      @users = User.order(:email_address)
+    end
+
+    def new
+      @user = User.new
+    end
+
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        redirect_to admin_users_path, notice: "Utilisateur « #{@user.email_address} » créé."
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    def edit
+      @user = User.find(params[:id])
+    end
+
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        redirect_to admin_users_path, notice: "Utilisateur « #{@user.email_address} » mis à jour."
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      user = User.find(params[:id])
+      if user == Current.user
+        redirect_to admin_users_path, alert: "Vous ne pouvez pas supprimer votre propre compte."
+      elsif User.count == 1
+        redirect_to admin_users_path, alert: "Impossible de supprimer le dernier compte administrateur."
+      else
+        user.destroy!
+        redirect_to admin_users_path, notice: "Utilisateur « #{user.email_address} » supprimé."
+      end
+    end
+
+    private
+
+    def user_params
+      permitted = params.require(:user).permit(:email_address, :password, :password_confirmation)
+      permitted.delete(:password) if permitted[:password].blank?
+      permitted.delete(:password_confirmation) if permitted[:password_confirmation].blank?
+      permitted
+    end
+  end
+end
