67d9a59489d8053ce82fc2f86f64683cfddd7f7a
Bolo Michelin <72155+bolom@users.noreply.github.com>
2026-09-10 15:03:20 -0400

    Limit GlitchTip reporting to production

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,5 @@
 Sentry.init do |config|
   config.dsn = Rails.application.credentials.dig(:glitchtip, :dsn)
+  config.enabled_environments = %w[production]
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
 end
